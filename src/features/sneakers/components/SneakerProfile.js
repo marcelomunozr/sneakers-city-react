1fa562afd667bba83a13502ecc3549278d0ee55d
Marcelo Enrique Munoz Rodriguez <memunozro.2brains@bancochile.cl>
2021-06-26 01:31:36 -0400

    feat: smejoras diseno y datos a mostrar

--- src/features/sneakers/components/SneakerProfile.js
+++ src/features/sneakers/components/SneakerProfile.js
@@ -19,19 +19,18 @@ const SneakerProfile = ({
         marca,
         fechaLanzamiento,
         precio,
-        descripcion,
     } = sneaker;
+    const formatPrice = precio.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     return (
-        <Col sm={6} md={4} lg={3}>
+        <Col sm={6} md={4} lg={4}>
             <Card className="mb-4">
                 <Card.Img variant="top" src={img} />
                 <Card.Body>
                     <Card.Title>{modelo}</Card.Title>
                     <Card.Text className="mb-1">{`${labels.SNEAKER.MARCA}: ${marca}`}</Card.Text>
                     <Card.Text className="mb-1">{`${labels.SNEAKER.FECHA}: ${fechaLanzamiento}`}</Card.Text>
-                    <Card.Text className="mb-1">{`${labels.SNEAKER.VALOR}: ${precio}`}</Card.Text>
-                    <Card.Text className="mb-4 descnike">{descripcion}</Card.Text>
-                    <Row>
+                    <Card.Text className="mb-4 txt-price">$ {formatPrice}</Card.Text>
+                    {/* <Row>
                         <Col xs="auto">
                             <Button
                                 variant="dark"
@@ -40,7 +39,7 @@ const SneakerProfile = ({
                                 {labels.BUTTONS.DELETE}
                             </Button>
                         </Col>
-                    </Row>
+                    </Row> */}
                 </Card.Body>
             </Card>
         </Col>
